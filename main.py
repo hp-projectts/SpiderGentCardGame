dd7cd57d78644a84a45c99b6fdd8f450fa815f87
Harrison Phipps <hftphipps@googlemail.com>
2026-08-12 17:05:34 +0100

    created charatcer list

---
+++ main.py
@@ -0,0 +1,15 @@
+#COMIC BOOK INSPIRED CARD GAME W/ GOTHIC AESTHETIC
+
+#CHARACTER LIST
+characters = [
+    {"name": "Spider-Gent",
+     "power": 88},
+    {"name": "Percival Parkinson",
+     "power": 19},
+    {"name": "Dr Ignatius Eel",
+     "power": 80},
+    {"name": "Baron the Hunter",
+     "power": 75}
+]
+
+print(characters[1])
