ff097a838f5b5518164572ac76bf2df9468e4242
Harrison Phipps <hftphipps@googlemail.com>
2026-08-13 18:58:42 +0100

    not printing score - work out why

--- main.py
+++ main.py
@@ -4,35 +4,42 @@ from battle import battle_cards
 
 player_score = 0
 computer_score = 0
+round_number = 1
 
-characters = load_characters() #run load_characters function from card.py and assigns characters to variable 
+while player_score < 5 and computer_score < 5:
 
-player_card = choose_card(characters)
-characters.remove(player_card)
-computer_card = choose_card(characters)  #randomly assigns player/computer card using choose card function
+    print(f"\n---- Round {round_number} ----\n")
 
-show_card(player_card, "Your")
+    characters = load_characters() #run load_characters function from card.py and assigns characters to variable 
 
-valid_stats = ["power","speed", "intelligence"]
+    player_card = choose_card(characters)
+    characters.remove(player_card)
+    computer_card = choose_card(characters)  #randomly assigns player/computer card using choose card function
 
-chosen_stat = input("Choose a stat (power, speed, intelligence): ").lower()
+    show_card(player_card, "Your")
 
-while chosen_stat not in valid_stats:
-    print("Invalid stat")
-    chosen_stat = input("Choose again:").lower()
-print()
-show_card(computer_card, "Computer")
+    valid_stats = ["power","speed", "intelligence"]
 
-winner = battle_cards(player_card, computer_card, chosen_stat)
+    chosen_stat = input("Choose a stat (power, speed, intelligence): ").lower()
 
-if winner == "player":
-    player_score += 1
-elif winner == "computer":
-    computer_score += 1
+    while chosen_stat not in valid_stats:
+        print("Invalid stat")
+        chosen_stat = input("Choose again:").lower()
+    print()
+    show_card(computer_card, "Computer")
 
-print("Score")
-print(f"Player: {player_score}")
-print(f"Computer: {computer_score}")        
+    winner = battle_cards(player_card, computer_card, chosen_stat)
+
+    if winner == "player":
+        player_score += 1
+    elif winner == "computer":
+        computer_score += 1
+
+    print("Score")
+    print(f"Player: {player_score}")
+    print(f"Computer: {computer_score}")   
+
+    round_number += 1     
 
 
 
