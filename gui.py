d435597820d414f2d610bc6d1f8e2525e3198336
Harrison Phipps <hftphipps@googlemail.com>
2026-08-23 14:59:14 +0100

    updated display card function

--- gui.py
+++ gui.py
@@ -1,27 +1,29 @@
 import tkinter as tk
 from card import load_characters, choose_card
 
-characters = load_characters()
+#LOAD GAME DATA
 
+characters = load_characters()
 player_card = choose_card(characters)
 
-
+# CREATE MAIN WINDOW
 
 window = tk.Tk()
 
 window.title("Spider-Gent: A Card Game")
 window.geometry("800x600")
 
+#TITLE
+
 title_label = tk.Label(window, text="Spider-Gent and the nefarious criminals of New Bristol")
 title_label.pack()
 
-card_image = tk.PhotoImage(
-    file=player_card["image"]
-)
-card_image = card_image.subsample(4, 4)
+#CARDS CONTAINER
 
 cards_container = tk.Frame(window)
 cards_container.pack()
+
+#PLAYER CARD
 
 player_card_frame = tk.Frame(
     cards_container,
@@ -36,6 +38,97 @@ player_card_frame.pack(
     side="left",
     padx=20
 )
+
+card_name_label = tk.Label(
+    player_card_frame, 
+    text=player_card["name"], 
+    font=("Arial", 20, "bold"),
+    bg="#e8e0d0"
+    )
+card_name_label.pack()
+
+#This frame holds images and stats side by side
+content_frame = tk.Frame(
+    player_card_frame,
+    bg="#e8e0d0"
+)
+content_frame.pack(pady=10)
+
+#Player image area
+image_frame = tk.Frame(
+    content_frame,
+    bg="#e8e0d0"
+)
+image_frame.pack(
+    side="left",
+    padx=10
+)
+card_image = tk.PhotoImage(
+    file=player_card["image"]
+)
+card_image = card_image.subsample(4, 4)
+image_label= tk.Label(
+    image_frame,
+    image=card_image
+)
+
+image_label.pack(pady=10)
+
+#Player stats area
+stats_frame = tk.Frame(
+    content_frame,
+    bg="#e8e0d0"
+)
+stats_frame.pack(
+    side="right", 
+    padx=10
+)
+power_label = tk.Label(
+    stats_frame, 
+    text=f"Power: {player_card['power']}",
+    font=("Arial", 10, "bold"),
+    bg="#e8e0d0"
+    )
+power_label.pack(anchor="w")
+
+speed_label = tk.Label(
+    stats_frame, 
+    text=f"Speed: {player_card['speed']}",
+    font=("Arial", 10, "bold"),
+    bg="#e8e0d0"
+    )
+speed_label.pack(anchor="w")
+
+intelligence_label = tk.Label(
+    stats_frame, 
+    text=f"Intelligence: {player_card['intelligence']}",
+    font=("Arial", 10, "bold"),
+    bg="#e8e0d0"
+    )
+intelligence_label.pack(anchor="w")
+
+#Player card info area
+
+info_label = tk.Label(
+    stats_frame, 
+    text=player_card["info"],
+    font=("Arial", 6, "italic"),
+    bg="#e8e0d0",
+    wraplength=350,
+    justify="center"
+    )
+info_label.pack()
+
+
+
+
+
+
+
+
+
+# COMPUTER CARD
+
 
 computer_card_frame = tk.Frame(
     cards_container,
@@ -58,13 +151,7 @@ computer_title_label = tk.Label(
     bg="#d3d3d3"
 )
 computer_title_label.pack()
-card_name_label = tk.Label(
-    player_card_frame, 
-    text=player_card["name"], 
-    font=("Arial", 20, "bold"),
-    bg="#e8e0d0"
-    )
-card_name_label.pack()
+
 
 computer_image_label = tk.Label (
     computer_card_frame,
@@ -80,83 +167,41 @@ computer_power_label = tk.Label(
     text="Power: ???",
     bg="#d3d3d3"
 )
+computer_power_label.pack(anchor="w")
 
 computer_speed_label = tk.Label(
     computer_card_frame,
     text="Speed: ???",
     bg="#d3d3d3"
 )
+computer_speed_label.pack(anchor="w")
 
 computer_intelligence_label = tk.Label(
     computer_card_frame,
     text="Intelligence = ???",
     bg="#d3d3d3"
 )
+computer_intelligence_label.pack(anchor="w")
 
-content_frame = tk.Frame(
-    player_card_frame,
-    bg="#e8e0d0"
-)
-content_frame.pack(pady=10)
+# FUNCTIONS 
 
-image_frame = tk.Frame(
-    content_frame,
-    bg="#e8e0d0"
-)
-image_frame.pack(side="left",padx=10)
+#~~~~~~GO FROM HERE~~~~~~
 
-stats_frame = tk.Frame(
-    content_frame,
-    bg="#e8e0d0"
-)
-stats_frame.pack(side="right", padx=10)
 
-image_label= tk.Label(
-    image_frame,
-    image=card_image
-)
 
-image_label.pack(pady=10)
 
-power_label = tk.Label(
-    stats_frame, 
-    text=f"Power: {player_card['power']}",
-    font=("Ariel", 10, "bold"),
-    bg="#e8e0d0"
-    )
-power_label.pack(anchor="w")
 
-speed_label = tk.Label(
-    stats_frame, 
-    text=f"Speed: {player_card['speed']}",
-    font=("Ariel", 10, "bold"),
-    bg="#e8e0d0"
-    )
-speed_label.pack(anchor="w")
 
-intelligence_label = tk.Label(
-    stats_frame, 
-    text=f"Intelligence: {player_card['intelligence']}",
-    font=("Ariel", 10, "bold"),
-    bg="#e8e0d0"
-    )
-intelligence_label.pack(anchor="w")
 
-info_label = tk.Label(
-    stats_frame, 
-    text=player_card["info"],
-    font=("Ariel", 6, "italic"),
-    bg="#e8e0d0"
-    )
-info_label.pack()
 
-display_card(player_card)
+
+
+
 
 def deal_new_card():
 
     new_card = choose_card(characters)
     display_card(new_card)
-    
 
    
     
@@ -164,22 +209,22 @@ def deal_new_card():
 
 def display_card(card):
     card_name_label.config(
-        text=new_card["name"]
+        text=card["name"]
         )
     info_label.config(
-        text=new_card["info"]
+        text=card["info"]
         )
     power_label.config(
-        text=f"Power: {new_card['power']}"
+        text=f"Power: {card['power']}"
         )
     speed_label.config(
-        text=f"Speed: {new_card['speed']}"
+        text=f"Speed: {card['speed']}"
         )
     intelligence_label.config(
-        text=f"Intelligence: {new_card["intelligence"]}"
+        text=f"Intelligence: {card['intelligence']}"
         )
     new_image = tk.PhotoImage(
-        file=new_card["image"]
+        file=card["image"]
         )
     new_image = new_image.subsample(4, 4)
     
@@ -187,6 +232,8 @@ def display_card(card):
         image=new_image
         )
     image_label.image = new_image #this keeps the image in memory
+
+display_card(player_card)
 
 deal_button = tk.Button(
     window,
